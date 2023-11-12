de43241392931bbfc5ce359263af89bf30ec2c7c
mtayyabUCF <muhammad.tayyab@ucf.edu>
2023-11-12 13:45:00 -0500

    Made some changes.

--- generate_latents.py
+++ generate_latents.py
@@ -1,4 +1,4 @@
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline, AutoencoderKL
 import torch
 import os
 from PIL import Image
@@ -13,8 +13,10 @@ if not os.path.exists(latents_folder):
     os.mkdir(latents_folder)
 
 # Load the model
-pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
-pipe = pipe.to("cuda")
+# pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae")
+vae = vae.to("cuda")
+vae = vae.half()
 
 # torch transform, to tensor and normalize
 transform = transforms.Compose([
@@ -24,8 +26,11 @@ transform = transforms.Compose([
 
 # load image names from folder
 image_names = os.listdir(image_folder)
+# keep average of min and max latents
+min_latents = []
+max_latents = []
 
-for i in range(1):
+for i in range(len(image_names)):
     # read image
     image_name = image_names[i]
     image_path = os.path.join(image_folder, image_name)
@@ -33,11 +38,21 @@ for i in range(1):
 
     image = transform(image).unsqueeze(0).half().to("cuda")
 
-    encoder_out = pipe.vae.encode(image)
+    encoder_out = vae.encode(image)
     latents = encoder_out['latent_dist'].mean
+    latents = latents.detach().cpu().squeeze(0).permute(1, 2, 0).numpy()
 
-    # display min and range of latents in a single line
-    print(f"min: {latents.min()}, max: {latents.max()}, range: {latents.max() - latents.min()}")
+    # keep track of min and max latents
+    min_latents.append(latents.min())
+    max_latents.append(latents.max())
+
+    # delete variables to free up memory
+
+    del latents
+    del image
+    del encoder_out
+
+    # print(torch.cuda.memory_summary(device=None, abbreviated=False))
 
 
     # normalize latents to [0, 1]
@@ -58,8 +73,7 @@ for i in range(1):
     # latent_image = Image.fromarray((latents * 255).astype(np.uint8))
     # latent_image.save(latent_path.replace(".npy", ".png"))
 
-
-
-
-
-
+# display average of min and max latents
+print(len(min_latents))
+print(f"min min: {np.min(min_latents)}, max max: {np.max(max_latents)}, max range: {np.max(max_latents) - np.min(min_latents)}")
+print(f"average min: {np.mean(min_latents)}, average max: {np.mean(max_latents)}, average range: {np.mean(max_latents) - np.mean(min_latents)}")
